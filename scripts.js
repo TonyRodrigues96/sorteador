88a1b2ddfb68c889cf5c193a35d4497394831466
TonyRodrigues96 <otniel.rodrigues@outlook.com>
2024-12-14 15:53:37 -0300

    alteração de valor min e máx no input

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 function geradorDeNumero() {
+
     const min = Math.ceil(document.getElementById('input1').value)
     const max = Math.floor(document.getElementById('input2').value)
     const resultado = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(resultado)}
+    if (min <= max) {alert(resultado)} 
+    else {alert('O segundo valor deve ser maior que o primeiro')} 
+
+}
